ca69e9168580f2f33e1a61eec9bb493badbd133c
Ltta113 <letruongtuanh113@gmail.com>
2024-06-28 18:08:58 +0700

    'Layout'

---
+++ src/components/AppBar/index.tsx
@@ -0,0 +1,20 @@
+import Box from '@mui/material/Box'
+import ModeSelect from '../ModeSelect'
+
+function AppBar() {
+  return (
+    <Box
+      sx={{
+        backgroundColor: 'primary.light',
+        width: '100%',
+        height: '48px',
+        display: 'flex',
+        alignItems: 'center'
+      }}
+    >
+      <ModeSelect />
+    </Box>
+  )
+}
+
+export default AppBar
