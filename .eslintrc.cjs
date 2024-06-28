ea4aedd9cb1e86dc3aa1b077683eac2478e15934
Ltta113 <letruongtuanh113@gmail.com>
2024-06-28 18:08:58 +0700

    'Layout'

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -18,7 +18,7 @@ module.exports = {
     'react-refresh',
     '@typescript-eslint',
   ],
-  rules: { 
+  rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
@@ -27,6 +27,12 @@ module.exports = {
     'react-hooks/exhaustive-deps': 'warn',
     'react/prop-types': 0,
     'react/display-name': 0,
+    'no-restricted-imports': [
+      'error',
+      {
+        'patterns': ['@mui/*/*/*']
+      }
+    ],
     'no-console': 1,
     'no-lonely-if': 1,
     'no-unused-vars': 1,
