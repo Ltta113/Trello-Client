f5340cd588816d66a0047457f41e7abef6839ce2
Ltta113 <letruongtuanh113@gmail.com>
2024-06-29 22:56:51 +0700

    Generate Fake Data

---
+++ src/pages/Boards/BoardContent/ListColumns/Column/ListCards/ListCards.tsx
@@ -0,0 +1,38 @@
+import Box from '@mui/material/Box'
+import Card from './Card/Card'
+import { ICard } from '~/apis/type'
+
+interface CarsProps {
+  cards: ICard[]
+}
+
+function ListCards({ cards }: CarsProps) {
+  return (
+    <Box
+      sx={{
+        p: '0 5px',
+        m: '0 5px',
+        display: 'flex',
+        flexDirection: 'column',
+        gap: 1,
+        overflowX: 'hidden',
+        overflowY: 'auto',
+        maxHeight: (theme) => `calc(
+              ${theme.trello.boardContentHeight} - 
+              ${theme.spacing(5)} - 
+              ${theme.trello.columnHeaderHeight} -
+              ${theme.trello.columnFooterHeight})`,
+        '&::-webkit-scrollbar-thumb': { backgroundColor: '#ced0da' },
+        '&::-webkit-scrollbar-thumb:hover': {
+          backgroundColor: '#bfc2cf'
+        },
+        '&::-webkit-scrollbar': { width: '7px' }
+      }}
+    >
+      {cards?.map((card) => <Card key={card._id} card={card}/>)}
+
+    </Box>
+  )
+}
+
+export default ListCards
---
+++ src/utils/sort.ts
@@ -0,0 +1,11 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+export const mapOrder = <T extends { [key: string]: any }>(originalArray: T[], orderArray: string[], key: string): T[] => {
+  if (!originalArray || !orderArray || !key) return []
+
+  const clonedArray = [...originalArray]
+  const orderedArray = clonedArray.sort((a, b) => {
+    return orderArray.indexOf(a[key]) - orderArray.indexOf(b[key])
+  })
+
+  return orderedArray
+}
